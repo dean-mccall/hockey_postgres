3d73b3f4afbff7f33d0ccaf19bdc24b726cff72b
Dean McCall <dean@themccallfamily.net>
2023-04-19 22:02:32 -0600

    career_statistic loading.  added identity columns

--- hockey_load/stage_model.py
+++ hockey_load/stage_model.py
@@ -1,16 +1,13 @@
 """Declare models and relationships."""
 import logging
 
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, String, Integer, Identity, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_utils import create_database, database_exists, drop_database
 
-Base = declarative_base()
-#  configure logging
-logging.basicConfig(
-    level = logging.INFO,
-    format = '%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+logger = logging.getLogger(__name__)
 
+Base = declarative_base()
 
 
 class Player(Base):
@@ -18,8 +15,9 @@ class Player(Base):
 
     __tablename__ = 'player'
 
+    player_id = Column(Integer, Identity(), primary_key = True)
     player_name = Column(String(255), nullable = True)
-    player_url = Column(String(255), nullable = True, primary_key = True)
+    player_url = Column(String(255), nullable = True)
     born = Column(String(255), nullable = True)
     height = Column(String(255), nullable = True)
     weight = Column(String(255), nullable = True)
@@ -64,7 +62,8 @@ class Team(Base):
 
     __tablename__ = 'team'
 
-    team_url = Column(String(255), nullable = True, primary_key = True)
+    team_id = Column(Integer, Identity(), primary_key = True)
+    team_url = Column(String(255), nullable = True)
     league_conference = Column(String(255), nullable = True)
     conference_division = Column(String(255), nullable = True)
     team_name = Column(String(255), nullable = True)
@@ -82,7 +81,8 @@ class CareerStatistic(Base):
 
     __tablename__ = 'career_statistic'
 
-    player_url = Column(String(255), nullable = True, primary_key = True)
+    career_statistic_id = Column(Integer, Identity(), primary_key = True)
+    player_url = Column(String(255), nullable = True)
     season = Column(String(255), nullable = True)
     team = Column(String(255), nullable = True)
     league = Column(String(255), nullable = True)
@@ -97,6 +97,41 @@ class CareerStatistic(Base):
     playoff_season_point_count = Column(String(255), nullable = True)
     playoff_season_penalty_minute_count = Column(String(255), nullable = True)
 
+    def __init__(
+        self,
+        player_url:str,
+        season:str,
+        team:str,
+        league:str,
+        regular_season_games_played_count:str,
+        regular_season_goal_count:str,
+        regular_season_assist_count:str,
+        regular_season_point_count:str,
+        regular_season_penalty_minute_count:str,
+        playoff_season_games_played_count:str,
+        playoff_season_goal_count:str,
+        playoff_season_assist_count:str,
+        playoff_season_point_count:str,
+        playoff_season_penalty_minute_count:str):
+
+        self.player_url = player_url
+        self.season = season
+        self.team = team
+        self.league = league
+        self.regular_season_games_played_count = regular_season_games_played_count
+        self.regular_season_goal_count = regular_season_goal_count
+        self.regular_season_assist_count = regular_season_assist_count
+        self.regular_season_point_count = regular_season_point_count
+        self.regular_season_penalty_minute_count = regular_season_penalty_minute_count
+        self.playoff_season_games_played_count = playoff_season_games_played_count
+        self.playoff_season_goal_count = playoff_season_goal_count
+        self.playoff_season_assist_count = playoff_season_assist_count
+        self.playoff_season_point_count = playoff_season_point_count
+        self.playoff_season_penalty_minute_count = playoff_season_penalty_minute_count
+
+    def __init__(self):
+        pass
+
 
 
 def get_engine():
@@ -110,14 +145,14 @@ def deploy_schema():
     if not database_exists(engine.url):
         create_database(engine.url)
         Base.metadata.create_all(engine)
-    logging.debug('schema deployed')
+    logger.debug('schema deployed')
 
 def undeploy_schema():
     """undeploy schema"""
     engine = get_engine()
     if database_exists(engine.url):
         drop_database(engine.url)
-    logging.info('schema undeployed')
+    logger.info('schema undeployed')
 
 
 def redeploy_schema():
